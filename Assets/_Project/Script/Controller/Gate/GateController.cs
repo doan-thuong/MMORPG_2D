81d111edeedeeb1fc0ccad77252edfb0a50cc1fd
ThuongDT <thuongdt@ezg.vn>
2026-01-16 11:51:26 +0700

    update logic load scene & update shadow player

--- Assets/_Project/Script/Controller/Gate/GateController.cs
+++ Assets/_Project/Script/Controller/Gate/GateController.cs
@@ -1,14 +1,21 @@
+using UnityEditor;
 using UnityEngine;
 
 public class GateController : MonoBehaviour
 {
     [SerializeField] private LayerMask layerMask;
+    public SceneAsset sceneAsset;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
+        if (sceneAsset == null)
+        {
+            Debug.LogError("Scene null");
+            return;
+        }
         if (GameUtil.IsInLayer(collision.gameObject, layerMask))
         {
-            SceneLoader.LoadScene(SceneName.Scene.SCENE_HOME);
+            SceneLoader.LoadScene(sceneAsset.name);
         }
     }
 }
